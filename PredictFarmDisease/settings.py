d2cb7aa34fe64060a3110ab78f4148a69868c7c7
Gopal Panigrahi <gopalpanigrahi99@gmail.com>
2021-07-25 21:47:27 +0530

    Added facility to send email and show management information

--- PredictFarmDisease/settings.py
+++ PredictFarmDisease/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "emailid"
+EMAIL_HOST_PASSWORD = "password"
